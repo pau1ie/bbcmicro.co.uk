5e4c23faf295c07559028e76f4b0c001aa14302f
pau1ie <huffton@googlemail.com>
2016-12-20 17:40:15 +0000

    Add file with required function

---
+++ web/includes/playlink.php
@@ -0,0 +1,26 @@
+<?php
+function get_playlink($image,$jsbeeb,$wsroot) {
+#  print_r($image);
+  if ($image['customurl'] === NULL ) {
+    if ($image['filename'] === NULL ) {
+      $ssd = null;
+    } else {
+      $ssd = 'gameimg/discs/' . $image["filename"];
+      if (file_exists($ssd)) {
+        $url = $jsbeeb . $wsroot . '/' . $ssd;
+      } else {
+        $url = NULL;
+      }
+    }
+  } else {
+    if ($image['customurl']=='NONE') {
+      $url=NULL;
+    } else {
+      $url = str_replace('%jsbeeb%',$jsbeeb,$image['customurl']);
+      $url = str_replace('%wsroot%',$wsroot,$url);
+    }
+  }
+#  echo "URL:".$url."\n";
+  return $url;
+}
+?>
