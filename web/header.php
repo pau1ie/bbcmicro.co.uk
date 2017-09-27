b085aa7813a713f387682205a1fbe73936af6e3c
pau1ie <huffton@googlemail.com>
2017-09-27 22:07:23 +0100

    Fix label for Secondary Genre

--- web/header.php
+++ web/header.php
@@ -115,7 +115,7 @@ function randomgame() {
 function refines($state) { ?>
      <h4>Only include matches on:</h4>
 <?php
-  $types=array('T'=>'Title','Y'=>'Year','P'=>'Publisher','A'=>'Author','G'=>'Primary Genre','S'=>'Subgenre','Z'=>'Series');
+  $types=array('T'=>'Title','Y'=>'Year','P'=>'Publisher','A'=>'Author','G'=>'Primary Genre','S'=>'Secondary Genre','Z'=>'Series');
   foreach ( $types as $tid => $type ) {
     $checked='';
     if (array_key_exists('only',$state) && count($state['only'])==0){
