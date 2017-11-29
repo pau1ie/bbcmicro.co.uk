99851002284f07a3d31f20af3ee7d93ffd5b5f27
pau1ie <huffton@googlemail.com>
2017-11-29 00:05:34 +0000

    Set httponly on session cookie try 2

--- web/includes/admin_session.php
+++ web/includes/admin_session.php
@@ -1,6 +1,9 @@
 <?php
-ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_httponly', True);
 session_start();
+$params = session_get_cookie_params();
+//print_r($params);
+setcookie("PHPSESSID", session_id(), 0, $params["path"], $params["domain"], $params["secure"], true );
 if (!array_key_exists('bbcmicro',$_SESSION)) {
 	header("Location: login.php");
 	exit;
