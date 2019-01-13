16ed8298c1cc543d107912c21bfb344f356b3758
Paul Houghton <paul_huffton@yahoo.co.uk>
2019-01-13 00:20:44 +0000

    Change file format in archive. Hopefully more machine readable

--- web/archdl.php
+++ web/archdl.php
@@ -2,12 +2,23 @@
 require 'includes/config.php';
 require 'includes/db_connect.php';
 require 'includes/playlink.php';
+require 'includes/extract_db.php';
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+ <meta name="robots" content="noindex">
+</head>
+<body>
+<p>
+<?php
 
 $sqla="select max(imgupdated) as dt from games";
 $asth = $db->prepare($sqla,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 $asth->execute();
 $ags=$asth->fetchAll();
-//print_r($ags);
 
 $sql="select g.id, g.title, i.filename as ifile, i.subdir as idir, s.subdir as sdir, s.filename as sfile from games g left join images i on g.id = i.gameid left join screenshots s on g.id = s.gameid order by g.title";
 
@@ -22,52 +33,41 @@ if ($sth->execute()) {
 }
 
 $cwd=getcwd();
-//echo $cwd . '<br/>';
 $zipf=$cwd . '/' . 'tmp/allfiles.zip';
-$dump=$cwd . '/' . 'tmp/db.sql';
 $stat=stat($zipf);
-//echo "<pre>";
-//print_r($stat);
-//echo "</pre>";
-//echo "<br/><br/>";
-//echo "<table><tr><th>Time: </th><th>" . strtotime($ags[0]['dt']) ."</th></tr>";
-//echo "<tr><th>File time: </th><th>".$stat['mtime'];
-//echo "</th></tr></table>";
+
 if ($stat && (strtotime($ags[0]['dt']) < $stat['mtime'])) {
    echo "Using cached file<br/>";
 } else {
-  exportDatabase(DB_HOST, DB_USER, DB_PASS, DB_NAME, $dump);
   $zip = new ZipArchive;
-  $rc=$zip->open($zipf, ZipArchive::CREATE);
+  $rc=$zip->open($zipf, Ziparchive::CREATE | ZipArchive::OVERWRITE);
 
   if ($rc === TRUE) {
     echo "Creating image zip file...<br/>";
     foreach ($res as $line) {
-//      print_r($line);
       $lfile=get_discloc($line['ifile'],$line['idir']);
       if ($lfile> " ") {
-//        echo $lfile . '<br/>';
-        $tf=substr($line['title'],0,1);
-        $zip->addFile($lfile, $tf . '/' . $line['ifile']);
+        $fn=get_fn($line['title'],$lfile,$line['id']);
+        $zip->addFile($lfile, $fn);
       }
     }
+    $text = get_data();
+    $zip->addFromString('00index.html',$text);
   } else {
     echo "<br/>Error" . $rc . '<br/>';
   }
+
   $zipf=$cwd . '/' . 'tmp/allscr.zip';
   $zip = new ZipArchive;
-  $rc=$zip->open($zipf, ZipArchive::CREATE);
+  $rc=$zip->open($zipf, Ziparchive::CREATE | ZipArchive::OVERWRITE);
 
   if ($rc === TRUE) {
     echo "Creating screenshot zip file...<br/>";
-//    print_r($res);
     foreach ($res as $line) {
-//      print_r($line);
       $lfile=get_scrshot($line['sfile'],$line['sdir']);
       if ($lfile> " ") {
-//        echo $lfile . '<br/>';
-        $tf=substr($line['title'],0,1);
-        $zip->addFile($lfile, $tf . '/' . $line['sfile']);
+        $fn=get_fn($line['title'],$lfile,$line['id']);
+        $zip->addFile($lfile, $fn);
       }
     }
   } else {
@@ -77,11 +77,20 @@ if ($stat && (strtotime($ags[0]['dt']) < $stat['mtime'])) {
 }
 echo "<a href='tmp/allfiles.zip'>All files(zip)</a><br>";
 echo "<a href='tmp/allscr.zip'>All screenshots(zip)</a><br>";
+echo "</p></body></html>";
 
-function exportDatabase($host, $user, $password, $database, $targetFilePath)
-{
-//    echo 'mysqldump --host '. $host .' --user '. $user .' --password='. $password .' '. $database .' --result-file='.$targetFilePath;
-    //returns true iff successfull
-    return exec('mysqldump --host '. $host .' --user '. $user .' --password='. $password .' '. $database .' --result-file='.$targetFilePath) === 0;
+function get_fn($otitle,$file,$id) {
+  $path_parts = pathinfo($file);
+  $title=preg_split('/[,(]/',$otitle)[0];
+  $title=preg_replace("/[^a-zA-Z0-9]+/","",$title);
+  $tf=substr($otitle,0,1);
+  if (is_numeric($tf)) {
+    $tf = '0';
+  } else {
+    $tf = strtoupper($tf);
+  }
+  $fn=$tf . '/' . $title.'-'.$id.'.'.$path_parts['extension'];
+  return $fn;
 }
+
 ?>
---
+++ web/includes/extract_db.php
@@ -0,0 +1,160 @@
+<?php
+
+function get_data () {
+global $db;
+$sql="select g.parent, g.id, g.title_article, g.title, g.year, g.genre, g.reltype, g.players_max, g.players_min, g.joystick, i.filename, r.name, g.save, g.hardware, g.electron, g.version, g.series, g.series_no, g.notes, g.compat_a, g.compat_b, g.compat_master from games g left join images i on g.id = i.gameid left join genres r on g.genre = r.id order by g.id";
+
+$sth = $db->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+if ($sth->execute()) {
+  $res = $sth->fetchAll();
+} else {
+  echo "Error:";
+  echo "\n";
+  $sth->debugDumpParams ();
+  $res=array();
+}
+
+$gsql="select g.name from genres g left join game_genre gg on gg.genreid = g.id where gg.gameid = ? order by gg.ord";
+$gsth = $db->prepare($gsql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$gsth->bindParam(1, $id, PDO::PARAM_INT);
+
+$asql="select a.name, a.alias from authors a left join games_authors ga on ga.authors_id = a.id where ga.games_id = ?";
+$asth = $db->prepare($asql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$asth->bindParam(1, $id, PDO::PARAM_INT);
+
+$psql="select a.name from publishers a left join games_publishers ga on ga.pubid = a.id where ga.gameid =?";
+$psth = $db->prepare($psql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$psth->bindParam(1, $id, PDO::PARAM_INT);
+
+$csql="select c.name from compilations c left join games_compilations gc on gc.compilations_id = c.id where gc.games_id =?";
+$csth = $db->prepare($csql,array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$csth->bindParam(1, $id, PDO::PARAM_INT);
+
+$text='<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <meta name="robots" content="noindex">
+</head>
+<body>
+<table><tr>
+<th>Id</th><th>Parent</th><th>Disc</th><th>title article</th><th>title</th><th>publisher</th><th>filename</th><th>Commercial</th><th>genre</th><th>genre2</th><th>year</th><th>author</th><th>save</th><th>joystick</th><th>Compilation</th><th>series/no</th><th>playersmin</th><th>playersmax</th><th>hardware</th><th>electron</th><th>version</th><th>A</th><th>B</th><th>M</th><th>Notes</th></tr>';
+
+foreach ($res as $line) {
+  // print_r($line);
+  $fp=explode('-',$line['filename']);
+  $id=$line['id'];
+  // Secondary Genres
+
+  $gsth->execute(); 
+  $sgs=$gsth->fetchAll();
+  $gen2=array();
+  // print_r($sgs);
+  foreach ($sgs as $sg) { 
+    $gen2[]=$sg['name'];
+  }
+
+  // Author
+
+  $asth->execute(); 
+  $ags=$asth->fetchAll();
+  $auths=array();
+  // print_r($ags);
+  foreach ($ags as $auth) { 
+    if ( empty($auth['alias']) ) {
+      $auths[]=$auth['name'];
+    }else{
+      $auths[]=$auth['name'] . ' (' . $auth['alias'] . ')';
+    }
+  }
+
+  // Publisher
+
+  $psth->execute(); 
+  $pgs=$psth->fetchAll();
+  $pubs=array();
+  // print_r($ags);
+  foreach ($pgs as $pub) {
+    $pubs[]=$pub['name'];
+  }
+
+  // Compilation
+
+  $csth->execute(); 
+  $cgs=$csth->fetchAll();
+  $comps=array();
+  // print_r($cgs);
+  foreach ($cgs as $comp) {
+    $comps[]=$comp['name'];
+  }
+
+  $ol=array();
+  $ol[]=strtoupper($line['id']);
+  $ol[]=strtoupper($line['parent']);
+  $ol[]=strtoupper($fp[0]); 	//Disc
+				// Title
+  $ol[]=$line['title_article'];
+  $ol[]=$line['title'];
+
+				// Publisher
+  $ol[]=implode(', ',$pubs);
+
+  $ol[]=$line['filename'];	// Filename
+
+  $ol[]=$line['reltype'];	// Release Type
+  $ol[]=$line['name'];		// Genre1
+
+  $ol[]=implode(', ',$gen2);	// Genre2
+
+  $ol[]=$line['year'];		// Year
+
+#  $ol[]=$line['notes'];	// Notes
+
+  $ol[]=implode(', ',$auths);	// Authors
+
+				// Save
+  if ( $line['save'] == 'D' or $line ['save'] =='T' ) {
+    $ol[]='ST'.$line['save'];
+  } else {
+    $ol[]=$line['save'];
+  }
+
+  $ol[]=$line['joystick'];	// Joystick
+
+  $ol[]=implode(', ',$comps);	// Compilation
+
+				// Series.no
+  $ol[]=trim($line['series'].' '.$line['series_no']);
+
+  $ol[]=$line['players_min'];	// Players
+  $ol[]=$line['players_max'];
+
+  $ol[]=$line['hardware'];	// Hardware
+
+				// Electron release
+  if ($line['electron'] == 'Y') {
+    $ol[]=$line['electron'].'es';
+  } else {
+    $ol[]=$line['electron'];
+  }
+
+  $ol[]=$line['version'];	// Version
+
+  $ol[]=$line['compat_a'];
+  $ol[]=$line['compat_b'];
+  $ol[]=$line['compat_master'];
+  $ol[]=htmlspecialchars($line['notes']);
+//  $ol[]='';
+
+  $ol2='<tr><td>'.implode('</td><td>',$ol).'</td></tr>';
+  $text=$text . $ol2 . "\n";
+
+}
+$text=$text . "
+</table>
+</body>
+</html>
+";
+return $text;
+}
+?>
