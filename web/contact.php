614f16d698628f67759dd61ebd504e51305e70e2
Paul Houghton <paul_huffton@yahoo.co.uk>
2019-01-26 21:09:20 +0000

    Add email form to the contact page.

--- web/contact.php
+++ web/contact.php
@@ -13,7 +13,7 @@ require 'includes/menu.php';
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title><?php echo $site_name?> - About us</title>
+    <title><?php echo $site_name?> - Contact us</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bs/css/bootstrap.min.css" rel="stylesheet">
@@ -50,12 +50,174 @@ require 'includes/menu.php';
     <div class="jumbotron">
       <div class="container">
         <h1><?php echo $site_name?></h1>
-        <p>Contact us at the <a href='http://stardot.org.uk/forums/viewforum.php?f=51' target='_blank'>StarDot forums</a></p>
+        <p>Contact us at the <a href='http://stardot.org.uk/forums/viewforum.php?f=51' target='_blank'>StarDot forums</a>, or email us using the form below.</p>
       </div>
     </div>
+    <div class="container"><p>
+<?php
+// OPTIONS - PLEASE CONFIGURE THESE BEFORE USE!
 
-      <hr>
+$yourEmail = CONTACT_EMAIL; // the email address you wish to receive these mails through
+$yourWebsite = WS_ROOT; // the name of your website
+$thanksPage = ''; // URL to 'thanks for sending mail' page; leave empty to keep message on the same page 
+$maxPoints = 4; // max points a person can hit before it refuses to submit - recommend 4
+$requiredFields = "name,email,comments"; // names of the fields you'd like to be required as a minimum, separate each field with a comma
 
+
+// DO NOT EDIT BELOW HERE
+$error_msg = array();
+$result = null;
+
+$requiredFields = explode(",", $requiredFields);
+
+function clean($data) {
+	$data = trim(stripslashes(strip_tags($data)));
+	return $data;
+}
+function isBot() {
+	$bots = array("Indy", "Blaiz", "Java", "libwww-perl", "Python", "OutfoxBot", "User-Agent", "PycURL", "AlphaServer", "T8Abot", "Syntryx", "WinHttp", "WebBandit", "nicebot", "Teoma", "alexa", "froogle", "inktomi", "looksmart", "URL_Spider_SQL", "Firefly", "NationalDirectory", "Ask Jeeves", "TECNOSEEK", "InfoSeek", "WebFindBot", "girafabot", "crawler", "www.galaxy.com", "Googlebot", "Scooter", "Slurp", "appie", "FAST", "WebBug", "Spade", "ZyBorg", "rabaz");
+
+	foreach ($bots as $bot)
+		if (stripos($_SERVER['HTTP_USER_AGENT'], $bot) !== false)
+			return true;
+
+	if (empty($_SERVER['HTTP_USER_AGENT']) || $_SERVER['HTTP_USER_AGENT'] == " ")
+		return true;
+	
+	return false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	if (isBot() !== false)
+		$error_msg[] = "No bots please! UA reported as: ".$_SERVER['HTTP_USER_AGENT'];
+		
+	// lets check a few things - not enough to trigger an error on their own, but worth assigning a spam score.. 
+	// score quickly adds up therefore allowing genuine users with 'accidental' score through but cutting out real spam :)
+	$points = (int)0;
+	
+	$badwords = array("adult", "beastial", "bestial", "blowjob", "clit", "cum", "cunilingus", "cunillingus", "cunnilingus", "cunt", "ejaculate", "fag", "felatio", "fellatio", "fuck", "fuk", "fuks", "gangbang", "gangbanged", "gangbangs", "hotsex", "hardcode", "jism", "jiz", "orgasim", "orgasims", "orgasm", "orgasms", "phonesex", "phuk", "phuq", "pussies", "pussy", "spunk", "xxx", "viagra", "phentermine", "tramadol", "adipex", "advai", "alprazolam", "ambien", "ambian", "amoxicillin", "antivert", "blackjack", "backgammon", "texas", "holdem", "poker", "carisoprodol", "ciara", "ciprofloxacin", "debt", "dating", "porn", "link=", "voyeur", "content-type", "bcc:", "cc:", "document.cookie", "onclick", "onload", "javascript");
+
+	foreach ($badwords as $word)
+		if (
+			strpos(strtolower($_POST['comments']), $word) !== false || 
+			strpos(strtolower($_POST['name']), $word) !== false
+		)
+			$points += 2;
+	
+	if (strpos($_POST['comments'], "http://") !== false || strpos($_POST['comments'], "www.") !== false)
+		$points += 2;
+	if (isset($_POST['nojs']))
+		$points += 1;
+	if (preg_match("/(<.*>)/i", $_POST['comments']))
+		$points += 2;
+	if (strlen($_POST['name']) < 3)
+		$points += 1;
+	if (strlen($_POST['comments']) < 15 || strlen($_POST['comments']) > 1500)
+		$points += 2;
+	if (preg_match("/[bcdfghjklmnpqrstvwxyz]{7,}/i", $_POST['comments']))
+		$points += 1;
+	// end score assignments
+
+	if ( !empty( $requiredFields ) ) {
+		foreach($requiredFields as $field) {
+			trim($_POST[$field]);
+			
+			if (!isset($_POST[$field]) || empty($_POST[$field]) && array_pop($error_msg) != "Please fill in all the required fields and submit again.\r\n")
+				$error_msg[] = "Please fill in all the required fields and submit again.";
+		}
+	}
+
+	if (!empty($_POST['name']) && !preg_match("/^[a-zA-Z-'\s]*$/", stripslashes($_POST['name'])))
+		$error_msg[] = "The name field must not contain special characters.\r\n";
+	if (!empty($_POST['email']) && !preg_match('/^([a-z0-9])(([-a-z0-9._])*([a-z0-9]))*\@([a-z0-9])(([a-z0-9-])*([a-z0-9]))+' . '(\.([a-z0-9])([-a-z0-9_-])?([a-z0-9])+)+$/i', strtolower($_POST['email'])))
+		$error_msg[] = "That is not a valid e-mail address.\r\n";
+	if (!empty($_POST['url']) && !preg_match('/^(http|https):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i', $_POST['url']))
+		$error_msg[] = "Invalid website url.\r\n";
+	
+	if ($error_msg == NULL && $points <= $maxPoints) {
+		$subject = "Automatic Form Email";
+		
+		$message = "You received this e-mail message through your website: \n\n";
+		foreach ($_POST as $key => $val) {
+			if (is_array($val)) {
+				foreach ($val as $subval) {
+					$message .= ucwords($key) . ": " . clean($subval) . "\r\n";
+				}
+			} else {
+				$message .= ucwords($key) . ": " . clean($val) . "\r\n";
+			}
+		}
+		$message .= "\r\n";
+		$message .= 'IP: '.$_SERVER['REMOTE_ADDR']."\r\n";
+		$message .= 'Browser: '.$_SERVER['HTTP_USER_AGENT']."\r\n";
+		$message .= 'Points: '.$points;
+
+		if (strstr($_SERVER['SERVER_SOFTWARE'], "Win")) {
+			$headers   = "From: $yourEmail\r\n";
+		} else {
+			$headers   = "From: $yourWebsite <$yourEmail>\r\n";	
+		}
+		$headers  .= "Reply-To: {$_POST['email']}\r\n";
+
+		if (mail($yourEmail,$subject,$message,$headers)) {
+			if (!empty($thanksPage)) {
+				header("Location: $thanksPage");
+				exit;
+			} else {
+				$result = 'Your mail was successfully sent.';
+				$disable = true;
+			}
+		} else {
+			$error_msg[] = 'Your mail could not be sent this time. ['.$points.']';
+		}
+	} else {
+		if (empty($error_msg))
+			$error_msg[] = 'Your mail looks too much like spam, and could not be sent this time. ['.$points.']';
+	}
+}
+function get_data($var) {
+	if (isset($_POST[$var]))
+		echo htmlspecialchars($_POST[$var]);
+}
+
+if (!empty($error_msg)) {
+	echo '<p class="bg-danger">ERROR: '. implode("<br />", $error_msg) . "</p>";
+}
+if ($result != NULL) {
+	echo '<p class="success">'. $result . "</p>";
+}
+?>
+
+
+
+<form role="form" action="<?php echo basename(__FILE__); ?>" method="post">
+<div class="form-group">
+<noscript>
+		<p><input type="hidden" name="nojs" id="nojs" /></p>
+</noscript>
+<div class="form-group">
+	<label for="name">Name: *</label> 
+	<input type="text" class="form-control" name="name" id="name" value="<?php get_data("name"); ?>" /><br />
+	
+</div>
+<div class="form-group">
+	<label for="email">E-mail: *</label> 
+	<input type="text" name="email" class="form-control" id="email" value="<?php get_data("email"); ?>" /><br />
+</div>
+	
+<div class="form-group">
+	<label for="comments">Comments: *</label>
+	<textarea name="comments" class="form-control" id="comments" rows="5" cols="20"><?php get_data("comments"); ?></textarea><br />
+</div>
+</p>
+<p>
+	<input type="submit" class="btn btn-primary btn-lg btn-block" name="submit" id="submit" value="Send" <?php if (isset($disable) && $disable === true) echo ' disabled="disabled"'; ?> />
+</p>
+</form>
+<p>&nbsp;</p>
+<p class="text-right"><small>Powered by <a href="http://jemsmailform.com/">Jem's PHP Mail Form</a></small></p>
+
+    </p></div>
 
 
     <!-- Bootstrap core JavaScript
